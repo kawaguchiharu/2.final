302afe106daf15678c4ef6963801bf463a0daf02
kawaguchiharu <s2322018@stu.musashino-u.ac.jp>
2024-01-14 13:42:45 +0900

    コード変更

--- 2code.py
+++ 2code.py
@@ -7,8 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 
-############### ダウンロード機能の追加 ###############
-# ダウンロードファイルの一時保存フォルダを作成(カレントディレクトリに「tmp」フォルダを作成)
+# ダウンロードファイルの一時保存フォルダを作成
 tmp_dir = Path(Path.cwd(), "tmp")
 tmp_dir.mkdir(exist_ok=True, parents=True)
 
@@ -18,11 +17,12 @@ prefs = {"download.default_directory": str(tmp_dir)}
 options.add_experimental_option("prefs", prefs)
 
 # chromedriverのパスを指定
-driver_path = "/Users/「PCのユーザ名」/Downloads/chromedriver"
+driver_path = "/Users/kawaguchiharu/Downloads/chromedriver"
 chrome_service = Service(executable_path=driver_path)
 
 # 追加したオプションを設定してwebdriverを起動
 driver = webdriver.Chrome(service=chrome_service, options=options)
 
-#参考
-#変更確認
+# e-Statsのcsvダウンロードページにアクセス
+url = "https://www.e-stat.go.jp/stat-search/files?page=1&layout=datalist&toukei=00130002&tstat=000001032793&cycle=7&year=20230&month=0&tclass1val=0"
+driver.get(url=url)
